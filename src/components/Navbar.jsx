b9adc3bf4667538d3a37c184a8d729bf52d26ff8
tega <kesienametitiri024@gmail.com>
2025-03-19 18:33:54 +0100

    add readme  cv update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { FaGithub, FaLinkedin, FaDownload } from "react-icons/fa";
-import CV from "../assets/Metitiri_Oghenetega_Cv7.pdf";
+import CV from "../assets/Metitiri_Oghenetega_Cv8.pdf";
 
 const Navbar = () => {
   return (
